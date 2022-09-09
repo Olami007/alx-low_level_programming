135c6510333aae9b1441876d86bf7bc3a4965533
olami007 <oladimejibells@gmail.com>
2022-09-08 17:20:42 -0700

    fnction printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - print the string in the put function
- *
+ * main - Entry point
+ * Description: C program that prints with printf function
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	print("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
