a8cdcc72fcc553008d47be195919abb81b5a0fb0
olami007 <oladimejibells@gmail.com>
2022-10-06 09:44:11 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -8,13 +8,13 @@
 
 void *malloc_checked(unsigned int b)
 {
-	void *p;
+void *p;
 
-	p = malloc(b);
-	if (p == NULL)
-	{
-		exit(98);
-	}
+p = malloc(b);
+if (p == NULL)
+{
+exit(98);
+}
 
-	return (p);
+return (p);
 }
