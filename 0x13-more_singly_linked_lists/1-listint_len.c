c3199b2ad4353d884d727caba27ac01de26e8513
Olami007 <oladimejibells@gmail.com>
2022-10-24 11:58:55 -0700

    4-free_listint.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,12 +3,12 @@
 
 /*
 * listint_len - Returns the number of elements
+*
 * @h: A pointer to the head of the listint_t list
 *
 * Return: The number of elements in the listint_t list
 */
 size_t listint_len(const listint_t *h)
-
 {
 	size_t nodes = 0;
 
